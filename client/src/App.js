f1cc0ab361eebd78291b35267ac969d6d796803b
Sun <ahn132@purdue.edu>
2023-05-15 19:42:16 -0400

    login template

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import Login from "./pages/Login";
+import Home from "./pages/Home"
+import Signup from "./pages/Signup";
+
+function App() {
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App
