af3b8df32d5c80798811f352d46ee9a6647d1cb0
HojinSohn <112189065+HojinSohn@users.noreply.github.com>
2023-05-23 23:20:52 +0900

    tw tp web scraped

--- web_scraper/science_req_scraper.py
+++ web_scraper/science_req_scraper.py
@@ -15,11 +15,52 @@ twtp_resp = requests.get("https://www.purdue.edu/science/Current_Students/curric
                          "-courses-in-technical-writing.html")
 twtp_soup = BeautifulSoup(twtp_resp.content, 'html5lib')
 
+container = twtp_soup.find('div', class_='maincontent col-lg-9 col-md-9 col-sm-9 col-xs-12 right')
+course_entries = container.find_all("p")
+filtered_course = [entry for entry in course_entries if "style13" not in entry.get('class', [])]
+index_save = 0
+for i in range(1, len(filtered_course)):
+    # print(filtered_course[i].getText())
+    row = filtered_course[i].getText()
+    if row.__contains__("course that meets"):
+        index_save = i
+        break
+    class_id = row[0:row.find(':')]
+    if (class_id.__contains__('(')):
+        class_id = row[0:row.find(' (')]
+    print(class_id)
+    # cursor.execute("""
+    #             UPDATE classesList SET ? = true WHERE class_id = ?
+    #         """, ["tw", class_id])
+
+
+# Approved courses in Technical Presentation:
+# Any course that meets the University Core Oral Communication (OC) requirement will also fulfill this requirement.
+# can we just refer to the core_oc instead of using this column? or I should create one..?
+
+for i in range(index_save + 1, len(filtered_course)):
+    row = filtered_course[i].getText()
+    if row.__contains__("course that meets"):
+        index_save = i
+        break
+    class_id = row[0:row.find(':')]
+    if (class_id.__contains__('(')):
+        class_id = row[0:row.find(' (')]
+    print(class_id)
+    # cursor.execute("""
+    #                 UPDATE classesList SET ? = true WHERE class_id = ?
+    #             """, ["tp", class_id])
+    # cursor.execute("""
+    #                     UPDATE classesList SET ? = true WHERE class_id = ?
+    #                 """, ["tw", class_id])
+
+
+
 # team
 # met by cs180
 
-# language and culture 
-# for the language courses, make an array of language course prefixes, 
+# language and culture
+# for the language courses, make an array of language course prefixes,
 # go through _01 and _02 in a for loop until you can't find any more
 
 lang_prefix = ["ARAB", "ASL", "CHNS", "FR", "GER", "GREK", "HEBR", "ITAL", "JPNS", "KOR", "LATN", "LC", "PTGS", "RUSS", "SPAN"]
@@ -52,7 +93,7 @@ for course in lang_cult_rows:
     # get subject name and split at "-"
     subject = data[0].getText()
     prefix = subject.split(" -")[0]
-    
+
     # get course number
     number = data[1].getText()
 
@@ -76,7 +117,7 @@ for course in issues_rows:
     # get subject name and split at "-"
     subject = data[0].getText()
     prefix = subject.split(" -")[0]
-    
+
     # get course number
     number = data[1].getText()
 
@@ -87,7 +128,7 @@ for course in issues_rows:
             #UPDATE classesList SET ? = true WHERE class_id = ?
         #""", ["sci_gis", class_id])
 
-# sts is same as core_sts except removing cs no count list 
+# sts is same as core_sts except removing cs no count list
 # (honestly put all the core_sts courses and sts and then go through no count list and set them back to NULL)
 
 sts_no_count = requests.get("https://www.purdue.edu/science/Current_Students/curriculum_and_degree_requirements/cos"
@@ -103,7 +144,7 @@ for course in no_sts_rows:
 
     # get subject name and split at "-"
     prefix = data[0].getText()
-    
+
     # get course number
     raw_number = data[1].getText()
     number = raw_number.split(" ")[0]
@@ -115,7 +156,6 @@ for course in no_sts_rows:
             #UPDATE classesList SET ? = false WHERE class_id = ?
         #""", ["sci_sts", class_id])
 
- #hello
 # lab science
 lab_resp = requests.get("https://www.purdue.edu/science/Current_Students/curriculum_and_degree_requirements/approved"
                         "-sequences-in-laboratory-science.html")
@@ -145,7 +185,7 @@ for course in gen_rows:
     # get subject name and split at "-"
     subject = data[0].getText()
     prefix = subject.split(" -")[0]
-    
+
     # get course number
     number = data[1].getText()
 
@@ -155,3 +195,4 @@ for course in gen_rows:
     #cursor.execute("""
             #UPDATE classesList SET ? = true WHERE class_id = ?
         #""", ["sci_gen", class_id])
+
